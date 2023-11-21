9df954621259f3bc68a3e29d1b28bb2b7f49e9d3
sanyam <sanyam22448@iiitd.ac.in>
2023-11-21 19:57:14 +0530

    le bete

--- stick_hero/src/main/java/com/example/stick_hero/HelloController.java
+++ stick_hero/src/main/java/com/example/stick_hero/HelloController.java
@@ -2,10 +2,17 @@ package com.example.stick_hero;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader; // Import the correct FXMLLoader class
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
@@ -16,6 +23,8 @@ public class HelloController {
     Button play;
     @FXML
     ImageView intro;
+    private Stage stage; // Declare Stage
+    private Scene scene; // Declare Scene
 
     @FXML
     public void initialize() {
@@ -23,9 +32,19 @@ public class HelloController {
         Image introimg = new Image(getClass().getResourceAsStream("/Assets/Intro.jpg"));
         intro.setImage(introimg);
         img.setImage(newimg);
-        System.out.println("Heo");
+        System.out.println("Hello");
     }
 
+    @FXML
+    public void switchToScene1(ActionEvent e) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("play-view.fxml"));
+        stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
     public void play(ActionEvent e) {
         System.out.println("Play");
     }
